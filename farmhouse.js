ab013fb4ef91fb5b55688bd0bcda88cfccc12e2d
Russell Schmidt <russell.schmidt@me.com>
2019-01-26 18:10:57 -0800

    first commit

---
+++ farmhouse.js
@@ -0,0 +1,125 @@
+var fmdata = [];
+
+const denverZip = [80237,
+  80236,
+  80239,
+  80238,
+  80243,
+  80246,
+  80247,
+  80250,
+  80249,
+  80261,
+  80263,
+  80266,
+  80271,
+  80291,
+  80123,
+  80202,
+  80201,
+  80204,
+  80203,
+  80206,
+  80205,
+  80208,
+  80207,
+  80210,
+  80209,
+  80212,
+  80211,
+  80217,
+  80216,
+  80219,
+  80218,
+  80221,
+  80220,
+  80223,
+  80222,
+  80224,
+  80227,
+  80231,
+  80230,
+  80235,
+]
+
+function getMarket() {
+  zipArray = denverZip;
+  getMarketList(zipArray);
+}
+
+
+function getMarketList(zipArray) {
+  for (var i = 0; i < zipArray.length; i++ ) {
+    getMarketId(zipArray[i])
+  }
+}
+
+function getMarketId(zip) {
+  const url = "https://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=" + zip;
+  fetch(url)
+    .then(res => res.json())
+    .then(data => data.results)
+    .then(data => {
+      for (var i = 0; i < data.length; i++) {
+        getMarketDetail(data[i].id, data[i].marketname)
+      }
+    })
+}
+
+function getMarketDetail(id, name = 'unavailable') {
+  const url = "https://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=" + id;
+  fetch(url)
+    .then(res => res.json())
+    .then(data => fmdata.push({name, ...data.marketdetails}))
+    .then(data => console.log(fmdata))
+}
+
+function convertArrayOfObjectsToCSV(args) {
+  var result, ctr, keys, columnDelimiter, lineDelimiter, data;
+
+  data = args.data || null;
+  if (data == null || !data.length) {
+    return null;
+  }
+
+  columnDelimiter = args.columnDelimiter || ',';
+  lineDelimiter = args.lineDelimiter || '\n';
+
+  keys = Object.keys(data[0]);
+
+  result = '';
+  result += keys.join(columnDelimiter);
+  result += lineDelimiter;
+
+  data.forEach(function(item) {
+    ctr = 0;
+    keys.forEach(function(key) {
+      if (ctr > 0) result += columnDelimiter;
+      result += item[key];
+      ctr++;
+    });
+    result += lineDelimiter;
+  });
+
+  return result;
+}
+
+function downloadCSV(args) {
+  var data, filename, link;
+  var csv = convertArrayOfObjectsToCSV({data: fmdata});
+
+  if (csv == null) return;
+
+  filename = args.filename || 'export.csv';
+  
+  if (!csv.match(/^data:text\/csv/i)) {
+    csv = 'data:text/csv;charset=utf-8,' + csv;
+  }
+
+  data = encodeURI(csv);
+
+  link = document.createElement('a');
+  link.setAttribute('href', data);
+  link.setAttribute('download', filename);
+  link.click();
+}
